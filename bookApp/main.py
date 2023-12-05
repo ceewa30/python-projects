eb8273cad662a5488af07ac35bdc66976f666d14
ceewa30 <ceewa30@gmail.com>
2023-12-05 14:28:46 -0500

    Chat bot initial commit

---
+++ bookApp/main.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+df = pd.read_csv("articles.csv")
+print(df)
+
+class User:
+    def customer_details(self):
+        pass
+
+class Store:
+    pass
+
+class Purchace:
+    pass
