f697174c5df2a0e80d6416c9ff59c42aef245436
ceewa30 <ceewa30@gmail.com>
2024-01-09 18:05:42 -0500

    adding stage lives in the game

--- hangmangame/main.py
+++ hangmangame/main.py
@@ -69,30 +69,38 @@ print(f"The solution is {chosen_word}.")
 
 
 wordlen = len(chosen_word)
+
+#TODO-1: - Create a variable called 'lives'to keep track of the number of lives left.
+#Set 'lives' to equal 6.
+lives = 6
+
 display = []
 for _ in range(wordlen):
     display += "_"
 print(display)
-# TODO-1: - Use a while loop to let the user guess again.  The loop should only stop once the user has gussed all the letters in the chosen_word and 'display' has no more blanks ("_").  Then you can tell the user they've won.
+
 end_of_game = False
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
-
-# TODO-2 : - Loop through each position in the chosen_word;
-# if the letter at that position matches 'guess' then reveal that letter in the display at that position.
-# e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
 
     for position in range(wordlen):
         letter = chosen_word[position]
         if letter == guess:
             display[position] = letter
+#TODO-2: - If guess is not a letter in the chosen_word, Then reduce 'lives' by 1.
+# If lives goes down to 0 then the game should stop and it should print "You lose."
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
 
-
-    # TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
-    # Hint - Don't worry about getting the user to guess the next letter. We'll tacke that in step 3.
-
+# Join all the elements in the list and turn it into a String.
     print(display)
 
     if "_" not in display:
         end_of_game = True
         print("You win.")
+
+#TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
+    print(stages[lives])
