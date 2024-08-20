bffacbda02d0bf3dd2f6167d9504e3e9ecec311c
ceewa30 <ceewa30@gmail.com>
2024-08-20 16:06:36 -0400

    Turtle road crossing game

---
+++ pong_vertical_game/paddle 2.py
@@ -0,0 +1,24 @@
+from turtle import Turtle
+from ball import Ball
+
+
+class Paddle(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_wid=1, stretch_len=5)
+        self.penup()
+        self.goto(0, -250)
+
+    def go_right(self):
+        if self.xcor() <= 340:
+            self.goto(self.xcor() + 20,self.ycor())
+
+    def go_left(self):
+        if self.xcor() >= -360:
+            self.goto(self.xcor() - 20,self.ycor())
+
+    def space(self):
+        self.ball.move()
